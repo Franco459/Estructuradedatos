c2e8b0d2599b2b9bd38223ef5fa32d4e6c53b7d7
Franco459 <francopistonecarp11@gmail.com>
2022-08-19 12:59:33 -0300

    Helper para TP0

---
+++ TP0/Helper.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP0;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author FrancoGP
+ */
+public class Helper {
+    
+    static Scanner entrada = new Scanner(System.in);
+    public static int forcePositiveIntEnter(String msg) {
+        int size = 0;
+        while(true){
+            System.out.println(msg);
+            try{
+                size = Integer.parseInt(entrada.nextLine());
+                if (size > 0) break;
+                else System.out.println("Validos numeros mayores que 0");
+            }
+            catch(NumberFormatException e){
+                System.out.println("SOLAMENTE VALIDOS NUMEROS ENTEROS POSITIVOS. "+e);
+            }
+        }
+        return size;
+    }
+    
+    public static int forceInteger(String msg){
+        int N = 0;
+        while(true){
+            System.out.println(msg);
+            try{
+                N = Integer.parseInt(entrada.nextLine());
+                break;
+            }
+            catch(NumberFormatException e){
+                System.out.println("SOLAMENTE VALIDOS NUMEROS ENTEROS. "+e);
+            }
+        }
+        return N;
+    }
+}
