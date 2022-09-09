1df64ebc78d3b748c26a81a05bca4ad2aeaf798b
Franco459 <francopistonecarp11@gmail.com>
2022-09-08 23:46:28 -0300

    Objeto libro, base, creado

---
+++ TP1/Libro.java
@@ -0,0 +1,63 @@
+package TP1;
+
+public class Libro {
+    private String title;
+    private String author;
+    private String editorial;
+    private String yearOfRelease;
+    private float price;
+    
+    public Libro(){
+
+    }
+
+    public Libro(String title, String author, String editorial, String yearOfRelease, float price){
+        this.title = title;
+        this.author = author;
+        this.editorial = editorial;
+        this.yearOfRelease = yearOfRelease;
+        this.price = price;
+    }
+
+    /***************/
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    
+    public String getAuthor(){
+        return this.author;
+    }
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+
+    public String getEditorial(){
+        return this.editorial;
+    }
+    public void setEditorial(String editorial){
+        this.editorial = editorial;
+    }
+
+
+    public String getYearOfRelease(){
+        return this.yearOfRelease;
+    }
+    public void setYearOfRelease(String yearOfRelease){
+        this.yearOfRelease = yearOfRelease;
+    }
+    
+
+    public float getPrice(){
+        return this.price;
+    }
+    public void setPrice(float price){
+        this.price = price;
+    }
+    
+}
